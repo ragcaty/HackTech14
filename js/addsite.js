78d0a58213839263835cc31d266511591a282247
maggil <maggi@kamnet.com>
2014-01-25 08:35:00 -0800

    Adding and printing!
    Compatibility with local storage needed for persistence of data. Also
    timer display needs to be done.

--- js/addsite.js
+++ js/addsite.js
@@ -1,9 +1,49 @@
+var sites = new Array();
+var count = 0
+
+function submitform()
+{
+	//document.write("Hello");
+	var text1 = document.getElementById('newsite').value;
+	var text2 = document.getElementById('number').value;
+	if (text1 != '' && text2 != '')
+	{
+		document.getElementById('error').innerHTML = '';
+		sites[count] = text1;
+		count = count + 1;
+		update();
+
+	}
+	if (text1 == '')
+	{
+		document.getElementById('error').innerHTML = "Site not specified.";
+	}
+	else if (text2 == '')
+	{
+		document.getElementById('error').innerHTML = "Allotted time not specified.";
+	}
+}
+
+//document.getElementById('qa').onClick = submitform();
+
 document.addEventListener('DOMContentLoaded', function () {
 	    document.getElementById('qa').addEventListener('click', submitform);
 });
-function submitform()
-	{
-		var text = document.getElementById('newsite').value;
-		document.getElementById('texts').innerHTML = text;
 
+
+
+
+function update()
+	{	
+		for (var i=0, len=sites.length; i<len; i++)
+		{
+			if (i > 0)
+			{
+				document.getElementById('texts').innerHTML += (sites[i]) + "<br />";
+			}
+			else document.getElementById('texts').innerHTML = (sites[i]) + "<br />";
+
+		}
+		document.getElementById('newsite').value = '';
+		document.getElementById('number').value = '';
 	}
